cf38b453800be22311f40ea4e2e271399b6f85a6
Hosein <taha.yas68@gmail.com>
2023-07-09 07:18:12 +0330

    edit and add some files

--- Qt code/Introduction/hello.py
+++ Qt code/Introduction/hello.py
@@ -5,6 +5,11 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QVBoxLayout
 
 # 2. Create an instance of QApplication
 app = QApplication(sys.argv)
